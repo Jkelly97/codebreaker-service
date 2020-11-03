c786c3314ecba9541b5e3f84ce247b36e95ed89c
jkelly97 <jmkelly2041@gmail.com>
2020-11-03 12:07:13 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
@@ -0,0 +1,131 @@
+package edu.cnm.deepdive.codebreaker.model.entity;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Entity
+@Table(
+    name = "user_profile",
+    indexes = {
+        @Index(columnList = "created"),
+        @Index(columnList = "connected")
+    }
+)
+public class User {
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "user_id", nullable = false, updatable = false,
+      columnDefinition = "CHAR(16) FOR BIT DATA")
+  private UUID id;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @NonNull
+  @Column(nullable = false, updatable = false, unique = true)
+  private String oauthKey;
+
+  @NonNull
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date connected;
+
+  @NonNull
+  @Column(nullable = false, unique = true)
+  private String displayName;
+
+  @NonNull
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "originator", cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  @OrderBy("started DESC")
+  private final List<Match> matchesOriginated = new LinkedList<>();
+
+  @NonNull
+  @OneToMany(mappedBy = "winner", fetch = FetchType.LAZY,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @OrderBy("deadline DESC")
+  private final List<Match> matchesWon = new LinkedList<>();
+
+  @NonNull
+  @ManyToMany(mappedBy = "players", fetch = FetchType.LAZY)
+  @OrderBy("deadline DESC")
+  private final List<Match> matches = new LinkedList<>();
+
+  @NonNull
+  public UUID getId() {
+    return id;
+  }
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  @NonNull
+  public String getOauthKey() {
+    return oauthKey;
+  }
+
+  public void setOauthKey(@NonNull String oauthKey) {
+    this.oauthKey = oauthKey;
+  }
+
+  @NonNull
+  public Date getConnected() {
+    return connected;
+  }
+
+  public void setConnected(@NonNull Date connected) {
+    this.connected = connected;
+  }
+
+  @NonNull
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(@NonNull String displayName) {
+    this.displayName = displayName;
+  }
+
+  @NonNull
+  public List<Match> getMatchesOriginated() {
+    return matchesOriginated;
+  }
+
+  @NonNull
+  public List<Match> getMatchesWon() {
+    return matchesWon;
+  }
+
+  @NonNull
+  public List<Match> getMatches() {
+    return matches;
+  }
+
+}
